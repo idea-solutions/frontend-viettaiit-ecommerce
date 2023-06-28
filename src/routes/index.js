6013ae6f145bb5549e34bad94c4fcc471f8f45a1
Viet Tai <viettaixca123@gmail.com>
2023-06-28 08:23:51 +0700

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import Management from "../pages/products/management";
 import DashBoard from "../pages/products/dashboard";
 import defaultRoutes from "../assets/defaultRoutes";
+import Order from "../pages/order";
+import Notfound from "../pages/notFound";
 // routes admin
 const privateRoutes = [
   { com: <Management />, path: defaultRoutes.productManagement },
   { com: <DashBoard />, path: defaultRoutes.dashboard },
+  { com: <Order />, path: defaultRoutes.orders },
+  { com: <Notfound />, path: "*" },
 ];
 
 export { privateRoutes };
