09e390cc8ee8822baa3ff1b5c6bea8d9324f47b4
Viet Tai <viettaixca123@gmail.com>
2023-06-28 10:55:44 +0700

    add more icon

---
+++ src/assets/icons/index.js
@@ -0,0 +1,31 @@
+import {
+  faCaretRight,
+  faSquareCaretLeft,
+  faTableColumns,
+  faBookmark,
+  faCartPlus,
+  faSignal,
+  faStarHalfStroke,
+  faUserTie,
+  faUser,
+  faComment,
+  faBell,
+  faArrowRightToBracket,
+  faSun,faMagnifyingGlass
+} from "@fortawesome/free-solid-svg-icons";
+export {
+  faCaretRight,
+  faSquareCaretLeft,
+  faTableColumns,
+  faBookmark,
+  faCartPlus,
+  faSignal,
+  faStarHalfStroke,
+  faUserTie,
+  faUser,
+  faComment,
+  faBell,
+  faArrowRightToBracket,
+  faSun,faMagnifyingGlass
+};
+
