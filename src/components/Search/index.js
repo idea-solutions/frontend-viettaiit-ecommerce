41fed9821cbd91520137b407f603418f71be619a
Viet Tai <viettaixca123@gmail.com>
2023-06-29 17:57:23 +0700

    Custom Component Searh Header

---
+++ src/components/Search/index.js
@@ -0,0 +1,57 @@
+// FRAMEWORKS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+// ICONS
+import { faSearch, faFire } from "../../assets/icons";
+
+// STYLES
+import "./search.scss";
+
+// ANIMATION SEARCH
+const search = {
+  initial: { height: 0, opacity: 0 },
+  focus: { height: "auto", opacity: 1 },
+  blur: { height: 0, opacity: 0 },
+  transition: { duration: 2, ease: "linear" },
+};
+
+// FAKE DATA SEARCH
+const listSuggestSearch = [
+  "IPhone 8",
+  "IPhone 11",
+  "IPad Gen 9",
+  "Airspod",
+  "Sạc nhanh",
+  "Cáp Type C",
+];
+function Search({ className }) {
+  const [isFocus, setIsFocus] = useState(false);
+  return (
+    <motion.div
+      initial="initial"
+      onFocus={() => setIsFocus(true)}
+      onBlur={() => setIsFocus(false)}
+      animate={isFocus ? "focus" : "blur"}
+      className={`search ${className}`}
+    >
+      <Form.Control type="text" placeholder="Tìm kiếm..." />
+      <FontAwesomeIcon className="search-icon" icon={faSearch} />
+      <motion.div variants={search} className="suggest">
+        <div className="suggest__top">
+          <FontAwesomeIcon icon={faFire} />
+          <span>TÌM KIẾM NHIỀU NHẤT</span>
+        </div>
+        <div className="suggest__bottom">
+          {listSuggestSearch.map((item, idx) => (
+            <span key={idx}>{item}</span>
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export default Search;
