51864340001af1fc3e03393918f4e0235d309c00
Viet Tai <viettaixca123@gmail.com>
2023-06-29 17:56:56 +0700

    Custom Component Quantity

---
+++ src/components/ButtonQuantity/index.js
@@ -0,0 +1,13 @@
+import './button-quantity.scss'
+function ButtonQuantity() {
+    return (  
+
+      <div className='button-quantity'>
+        <button>-</button>
+        <span>3</span>
+        <button>+</button>
+      </div>
+    );
+}
+
+export default ButtonQuantity;
