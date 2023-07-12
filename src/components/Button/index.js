21e08853e7a513195d2eeb098f9fb4c50a69c98f
Viet Tai <viettaixca123@gmail.com>
2023-07-12 15:25:21 +0700

    Update button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,14 @@
 import "./button.scss";
-function Button({ children, type }) {
-  return <button className={`btn-custom__${type} btn btn-${type}`}>{children}</button>;
+function Button({ children, type, classes }) {
+  return (
+    <button
+      className={`btn-custom__${type} btn btn-${type} ${
+        classes ? classes : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
