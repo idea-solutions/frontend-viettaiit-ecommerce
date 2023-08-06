533ba2db87c7bab3971847eab5b605545141230a
Viet Tai <viettaixca123@gmail.com>
2023-08-06 09:53:30 +0700

    Fix bug : react-helmet warning error

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from "./App";
 import "./style.scss";
 import { Provider } from "react-redux";
 import store from "./store";
+import {  HelmetProvider } from "react-helmet-async";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>
 );
