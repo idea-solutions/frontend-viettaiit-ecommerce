55313421ea337bffe0bd1887a626e77504101e60
Viet Tai <viettaixca123@gmail.com>
2023-08-06 11:56:24 +0700

    tách Thanh Breadbrumb -> Component tái sử dụng layout

---
+++ src/components/Breadcrumb/index.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { publicRoutes } from "../../routes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+import "./breadcrumb.scss";
+function Breadcrumb({ title }) {
+  return (
+    <div className="container-fuild breadcrumb__breadcrumb">
+      <div className="container ">
+        <Link
+          to={publicRoutes.home}
+          className="breadcrumb__fs-13 hover-color-secondary text-decoration-none"
+        >
+          Trang chủ
+        </Link>
+        <FontAwesomeIcon
+          className="px-3 breadcrumb__fs-13"
+          icon={faAngleRight}
+        />
+        <span className="text-secondary breadcrumb__fs-13">{title}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumb;
