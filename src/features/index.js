3547938131f26133ebee17a5d4ed54928a58e98e
Viet Tai <viettaixca123@gmail.com>
2023-09-09 16:43:38 +0700

    get categories from server and render

--- src/features/index.js
+++ src/features/index.js
@@ -3,11 +3,13 @@ import authSlice from "./auth/authSlice";
 import loadingSlice from "./loadingSlice";
 import { combineReducers } from "redux";
 import productSlice from "./product/productSlice";
+import categorySlice from "./category/categorySlice";
 const rootReducer = combineReducers({
   navBar: navBarSlice,
   auth: authSlice,
   loading: loadingSlice,
   product: productSlice,
+  category: categorySlice,
 });
 
 export default rootReducer;
--- src/pages/home/danhMucNoiBat.js
+++ src/pages/home/danhMucNoiBat.js
@@ -2,9 +2,16 @@ import { Col } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Scrollbar } from "swiper/modules";
 import LazyImage from "../../components/LazyImage";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../../features/category/categorySlice";
 function DanhMucNoiBat() {
   console.log("[HOME] danhmucnoibat --- re-render");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+  const { categories } = useSelector((store) => store.category);
   return (
     <div className="container mt-5 danhmucnoibat">
       <h3 className="text-uppercase text-center mb-4">DANH MỤC NỔI BẬT</h3>
@@ -33,72 +40,25 @@ function DanhMucNoiBat() {
           },
         }}
       >
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Col className="item">
-            <div className="border-1 d-flex flex-column gap-1 align-items-center">
-              <LazyImage
-                src="https://bizweb.dktcdn.net/100/480/632/themes/900313/assets/cate_1.png?1691030040351    "
-                alt=""
-              />
-              <span className="mt-2 text-size-16">Iphone</span>
-            </div>
-          </Col>
-        </SwiperSlide>
+        {categories.map((category, index) => (
+          <SwiperSlide key={index}>
+            <Col className="item">
+              <div className="border-1 d-flex flex-column gap-1 align-items-center">
+                <LazyImage
+                  src={
+                    process.env.REACT_APP_BACKEND_URL +
+                    "/static/assets/images/category/" +
+                    category.image
+                  }
+                  alt={category.categoryName}
+                />
+                <span className="mt-2 text-size-16">
+                  {category.categoryName}
+                </span>
+              </div>
+            </Col>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
