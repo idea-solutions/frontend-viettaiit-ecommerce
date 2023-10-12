33a77b4882b857914a50d6af13f044ff5073f075
Viet Tai <viettaixca123@gmail.com>
2023-10-12 17:17:53 +0700

    Tack form address

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -3,87 +3,29 @@ import HelmetCustom from "../../components/HelmetCustom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useNavigate } from "react-router-dom";
 import { clientRoutes } from "../../routes";
-import { faCaretDown, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 import LazyImage from "../../components/LazyImage";
 import { useDispatch, useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/format";
 import { calculatePriceForDiscount } from "../../utils/calculatePrice";
-import { useEffect, useRef, useState } from "react";
-import useHideOnClickOutside from "../../hooks/useHideOnClickOutSide";
+
 import { toastInfo, toastSuccess } from "../../utils/toast";
-import axios from "axios";
 import { checkPhoneNumber } from "../../utils/validate";
 import { addOrderMe, getOrdersMe } from "../../features/order/orderSlice";
 import { setIsLoadingComp } from "../../features/loadingCompSlice";
+import FormAddress from "../../components/Form/FormAddress";
+import { resetFormAddress } from "../../features/formAddressSlice";
 function CheckOut() {
   const { cart, countCartItem, total } = useSelector((store) => store.cart);
-
-  const { user } = useSelector((store) => store.auth);
-  const [addresses, setAddresses] = useState(null);
-  const cityRef = useRef();
-  const districtRef = useRef();
-  const wardRef = useRef();
-  const [isShowSelectOptionCity, setIsShowSelectOptionCity] =
-    useHideOnClickOutside(cityRef);
-  const [isShowSelectOptionDistrict, setIsShowSelectOptionDistrict] =
-    useHideOnClickOutside(districtRef);
-  const [isShowSelectOptionWard, setIsShowSelectOptionWard] =
-    useHideOnClickOutside(wardRef);
-  const [city, setCity] = useState();
-  const [district, setDistrict] = useState(null);
-  const [ward, setWard] = useState(null);
-  const [info, setInfo] = useState({
-    phoneNumber: "",
-    province: "",
-    fullName: "",
-    address: "",
-    district: "",
-    ward: "",
-    country: "VN",
-    note: "",
-  });
-
-  const handleChange = (e) => {
-    setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-  useEffect(() => {
-    const getAddressesAsync = async () => {
-      try {
-        const { data } = await axios.get(
-          "https://provinces.open-api.vn/api/?depth=3"
-        );
-        setAddresses(data);
-        localStorage.setItem("addresses", JSON.stringify(data));
-      } catch (error) {}
-    };
-    const data = JSON.parse(localStorage.getItem("addresses"));
-    if (data) setAddresses(data);
-    else getAddressesAsync();
-  }, []);
-  const handleSelectCity = (city) => {
-    setCity(city);
-    setDistrict(null);
-    setWard(null);
-  };
-  const handleSelectDistrict = (district) => {
-    setDistrict(district);
-    setWard(null);
-  };
-
-  const handleSelectWard = (ward) => {
-    setWard(ward);
-  };
+  const { address } = useSelector((store) => store.formAddress);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { fullName, phoneNumber } = info;
+    const { fullName, phoneNumber } = address;
     if (fullName.length < 3) return toastInfo("Họ và tên yêu cầu!");
     if (!checkPhoneNumber(phoneNumber))
       return toastInfo("Định dạng số điện thoại không hợp lệ!");
-    info.province = city.name;
-    info.district = district.name;
-    info.ward = ward.name;
     const productItems = [];
     const ordersLine = [];
     cart.cartItems.forEach((item) => {
@@ -115,7 +57,7 @@ function CheckOut() {
     const inputs = {
       productItems,
       ordersLine,
-      ...info,
+      address,
     };
     dispatch(setIsLoadingComp(true));
     const { payload } = await dispatch(addOrderMe(inputs));
@@ -125,6 +67,7 @@ function CheckOut() {
       navigate(clientRoutes.checkout + "/cam-on/" + payload.data.id);
     }
     dispatch(setIsLoadingComp(false));
+    dispatch(resetFormAddress());
   };
 
   return (
@@ -156,180 +99,7 @@ function CheckOut() {
                     <FontAwesomeIcon icon={faUser} className="ms-2" /> Đăng nhập
                   </Link> */}
                 </div>
-                <Form className="">
-                  <FloatingLabel
-                    controlId="floatingInputGrid"
-                    label="Email"
-                    className="mb-2"
-                    disabled
-                  >
-                    <Form.Control
-                      size="sm"
-                      type="email"
-                      value={user.email}
-                      disabled
-                      placeholder="name@example.com"
-                    />
-                  </FloatingLabel>
-                  <FloatingLabel
-                    controlId="floatingInputGrid"
-                    label="Họ và tên"
-                    className="mb-2"
-                  >
-                    <Form.Control
-                      type="text"
-                      placeholder="Nguyen Viet Tai"
-                      value={info?.fullName}
-                      name="fullName"
-                      onChange={handleChange}
-                    />
-                  </FloatingLabel>
-
-                  <div className="d-flex mb-2">
-                    <FloatingLabel
-                      controlId="floatingInputGrid"
-                      label="Số điện thoại (tùy chọn)"
-                      className="w-100"
-                    >
-                      <Form.Control
-                        type="text"
-                        placeholder="329638260"
-                        value={info?.phoneNumber}
-                        name="phoneNumber"
-                        onChange={(e) => {
-                          // if (info.phoneNumber.length >= 10) return;
-                          handleChange(e);
-                        }}
-                      />
-                    </FloatingLabel>
-                  </div>
-                  <FloatingLabel
-                    controlId="floatingInputGrid"
-                    label="Địa chỉ (tùy chọn)"
-                    className="mb-2"
-                  >
-                    <Form.Control
-                      type="text"
-                      placeholder="124 Nguyen Tat Thanh"
-                      value={info?.address}
-                      name="address"
-                      onChange={handleChange}
-                    />
-                  </FloatingLabel>
-                  <div className="d-flex mb-2 position-relative" ref={cityRef}>
-                    <FloatingLabel
-                      controlId="floatingInputGrid"
-                      label="Tỉnh thành"
-                      className="w-100 d-flex"
-                    >
-                      <Form.Control
-                        type="text"
-                        placeholder="name@example.com"
-                        value={city?.name ? city.name : ""}
-                        onFocus={() => setIsShowSelectOptionCity(true)}
-                      ></Form.Control>{" "}
-                      <div
-                        className="border rounded-2 px-3 flex-center cursor"
-                        onClick={() => setIsShowSelectOptionCity(true)}
-                      >
-                        <FontAwesomeIcon icon={faCaretDown} />
-                      </div>
-                    </FloatingLabel>
-                    {isShowSelectOptionCity && (
-                      <SelectOptions
-                        setIsShow={setIsShowSelectOptionCity}
-                        addresses={addresses}
-                        handleSelect={handleSelectCity}
-                      ></SelectOptions>
-                    )}
-                  </div>
-                  <div
-                    className="d-flex mb-2 position-relative"
-                    ref={districtRef}
-                  >
-                    <FloatingLabel
-                      controlId="floatingInputGrid"
-                      label="Quận huyện"
-                      className="w-100 d-flex"
-                    >
-                      <Form.Control
-                        type="text"
-                        placeholder="name@example.com"
-                        value={district?.name ? district.name : ""}
-                        onFocus={() => {
-                          if (!city)
-                            return toastInfo("Vui lòng chọn tỉnh/thành");
-                          setIsShowSelectOptionDistrict(true);
-                        }}
-                      ></Form.Control>{" "}
-                      <div
-                        className="border rounded-2 px-3 flex-center cursor"
-                        onClick={() => {
-                          if (!city)
-                            return toastInfo("Vui lòng chọn tỉnh/thành");
-                          setIsShowSelectOptionDistrict(true);
-                        }}
-                      >
-                        <FontAwesomeIcon icon={faCaretDown} />
-                      </div>
-                    </FloatingLabel>
-                    {isShowSelectOptionDistrict && (
-                      <SelectOptions
-                        setIsShow={setIsShowSelectOptionDistrict}
-                        addresses={city?.districts}
-                        handleSelect={handleSelectDistrict}
-                      ></SelectOptions>
-                    )}
-                  </div>
-                  <div className="d-flex mb-2 position-relative" ref={wardRef}>
-                    <FloatingLabel
-                      controlId="floatingInputGrid"
-                      label="Phường xã"
-                      className="w-100 d-flex"
-                    >
-                      <Form.Control
-                        type="text"
-                        placeholder="name@example.com"
-                        value={ward?.name ? ward.name : ""}
-                        onFocus={() => {
-                          if (!district)
-                            return toastInfo("Vui lòng chọn huyện");
-                          setIsShowSelectOptionWard(true);
-                        }}
-                      ></Form.Control>{" "}
-                      <div
-                        className="border rounded-2 px-3 flex-center cursor"
-                        onClick={() => {
-                          if (!district)
-                            return toastInfo("Vui lòng chọn huyện");
-                          setIsShowSelectOptionWard(true);
-                        }}
-                      >
-                        <FontAwesomeIcon icon={faCaretDown} />
-                      </div>
-                    </FloatingLabel>
-                    {isShowSelectOptionWard && (
-                      <SelectOptions
-                        setIsShow={setIsShowSelectOptionWard}
-                        addresses={district?.wards}
-                        handleSelect={handleSelectWard}
-                      ></SelectOptions>
-                    )}
-                  </div>
-                  <FloatingLabel
-                    controlId="floatingTextarea2"
-                    label="Ghi chú (tùy chọn)"
-                  >
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Ghi chú"
-                      style={{ height: "70px" }}
-                      value={info?.note}
-                      name="note"
-                      onChange={handleChange}
-                    />
-                  </FloatingLabel>
-                </Form>
+                <FormAddress />
               </Col>
               <Col md={12} xl={6} className="">
                 <div className=" mt-sm-5 ">
@@ -473,23 +243,3 @@ function CheckOut() {
 }
 
 export default CheckOut;
-
-function SelectOptions({ addresses, handleSelect, setIsShow }) {
-  if (!addresses) return null;
-  return (
-    <div className="select_options scrollbar-primary">
-      <span>---</span>
-      {addresses.map((address, index) => (
-        <span
-          key={index}
-          onClick={() => {
-            handleSelect(address);
-            setIsShow(false);
-          }}
-        >
-          {address?.name}
-        </span>
-      ))}
-    </div>
-  );
-}
