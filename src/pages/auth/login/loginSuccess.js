9f39ff1ec234872b6926286bd2e2c02f71f5692c
Viet Tai <viettaixca123@gmail.com>
2023-10-17 23:23:15 +0700

    fix login facebook

--- src/pages/auth/login/loginSuccess.js
+++ src/pages/auth/login/loginSuccess.js
@@ -2,7 +2,6 @@ import { useDispatch } from "react-redux";
 import { getUserSuccess } from "../../../features/auth/authSlice";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { toastSuccess } from "../../../utils/toast";
 import { setIsLoadingComp } from "../../../features/loadingCompSlice";
 
 function LoginSuccess() {
@@ -13,8 +12,8 @@ function LoginSuccess() {
     dispatch(setIsLoadingComp(true));
     const getUserAsync = async () => {
       const { payload } = await dispatch(getUserSuccess());
+      console.log(payload);
       if (payload.status === 200) {
-        toastSuccess(payload.message);
         navigate("/");
       }
     };
